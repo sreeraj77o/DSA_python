194e51673ecc5b5c43e4f3050fd599f6028cb4e8
sree0077 <sreerajkasaragod007@gmail.com>
2025-09-03 17:04:15 +0530

    longest sub string

---
+++ topics/sliding_window/longestsubstring.py
@@ -0,0 +1,21 @@
+def longestsunstring(s,k):
+    max_length = 0
+    window_start = 0
+    char_frequency = {}
+    for window_end in range(len(s)):
+        right_char = s[window_end]
+        char_frequency[right_char] = char_frequency.get(right_char,0) + 1
+
+        while len(char_frequency)>k:
+            left_char = s[window_start]
+            char_frequency[left_char] -= 1
+            if char_frequency[left_char] == 0:
+                del char_frequency[left_char]
+            window_start +=1
+
+        max_length = max(max_length,window_end - window_start + 1)
+
+    return max_length
+
+s = 'araaci'
+print(longestsunstring(s,2))
